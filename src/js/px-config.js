5adbe918fca50bc04f69195e1790e87b20acd3c4
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-03-20 15:57:07 -0300

    style(px-config): ajustar código fonte

--- src/js/px-config.js
+++ src/js/px-config.js
@@ -10,7 +10,7 @@ define(['angular'], function(ng) {
 			PROJECT_ID: 1, // Identificação do projeto (table: px.project)
 			PROJECT_NAME: 'Phoenix Project - Exemplo', // Nome do projeto
 			PROJECT_SRC: 'px-example/src/', // Source do projeto
-			PROJECT_CSS: [PX_PACKAGE + 'system/login/login.css','styles.css'], // Arquivos .css
+			PROJECT_CSS: [PX_PACKAGE + 'system/login/login.css', 'styles.css'], // Arquivos .css
 			PROJECT_DSN: 'px_project_sql', // Data Source Name (CF)
 			LOCALE: 'pt-BR', // Locale
 			LOGIN_REQUIRED: true, // Login obrigatório?
@@ -19,8 +19,8 @@ define(['angular'], function(ng) {
 			GROUP_ITEM_SUFFIX: '', // Sufixo do campo GROUP_ITEM
 			GROUP_LABEL_SUFFIX: '', // Sufixo do campo GROUP_LABEL
 			GROUP_REPLACE: [], // Substituir no nome do campo GROUP
-			GROUP_ITEM:'grupo_id', // Idetificador de GROUP (Utilizar quando GROUP_ITEM_SUFFIX === '')
-			GROUP_LABEL:'grupo_nome' // Label do GROUP (Utilizar quando GROUP_LABEL_SUFFIX === '')
+			GROUP_ITEM: 'grupo_id', // Idetificador de GROUP (Utilizar quando GROUP_ITEM_SUFFIX === '')
+			GROUP_LABEL: 'grupo_nome' // Label do GROUP (Utilizar quando GROUP_LABEL_SUFFIX === '')
 		})
 
 	return config;
